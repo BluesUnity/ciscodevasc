8b022f72a2ca2870c4c300970719accd3d1c3bee
BluesUnity <93047050+BluesUnity@users.noreply.github.com>
2024-02-01 12:17:15 +0100

    Update and rename prog.sh to progTP2.sh

--- progTP2.sh
+++ progTP2.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+#ask ip csr
+read -p "Entrez l'adresse IP pour CSR1kv: " ip
 # Configuration des variables d'environnement pour le proxy
 export http_proxy=http://cache.univ-pau.fr:3128
 export https_proxy=$http_proxy
@@ -17,12 +18,12 @@ fi
 # Se déplacer dans le répertoire de travail
 cd "$DIRECTORY" || { echo "Erreur lors de l'accès au répertoire $DIRECTORY"; exit 1; }
 
-# Vérifier et créer le dossier backup si nécessaire
-if [ ! -d "backup" ]; then
-    echo "Création du dossier backup"
-    mkdir backup || { echo "Erreur lors de la création du dossier backup"; exit 1; }
+# Vérifier et créer le dossier backups si nécessaire
+if [ ! -d "backups" ]; then
+    echo "Création du dossier backups"
+    mkdir backups || { echo "Erreur lors de la création du dossier backups"; exit 1; }
 else
-    echo "Le dossier backup existe déjà."
+    echo "Le dossier backups existe déjà."
 fi
 
 # Affichage de la version d'Ansible
@@ -74,6 +75,19 @@ EOF
 else
     echo "Le fichier $FILENAME existe déjà. Aucune action n'est effectuée."
 fi
+
+HOSTS_FILE="$DIRECTORY/hosts"
+HOST_LINE="CSR1kv ansible_user=cisco ansible_password=cisco123! ansible_host=$ip"
+
+if ! grep -q "$HOST_LINE" "$HOSTS_FILE" 2>/dev/null; then
+    echo "Ajout de la configuration CSR1kv dans le fichier hosts."
+    echo "$HOST_LINE" >> "$HOSTS_FILE"
+else
+    echo "La configuration CSR1kv existe déjà dans le fichier hosts."
+fi
+
+#install package 
+pip install paramiko
 
 # Exécuter la commande ansible-playbook si toutes les étapes précédentes ont réussi
 echo "Exécution de ansible-playbook backup_cisco_router_playbook.yaml -i hosts"
